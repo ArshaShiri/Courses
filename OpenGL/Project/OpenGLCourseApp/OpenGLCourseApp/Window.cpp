a39602a3330166d38e819a0b8aa088fbb22ffff1
Arsha <arsha.shiri@gmail.com>
2020-05-16 15:02:44 +0200

    Added ability to the window class to detect mouse movement and keyboard input

--- OpenGL/Project/OpenGLCourseApp/OpenGLCourseApp/Window.cpp
+++ OpenGL/Project/OpenGLCourseApp/OpenGLCourseApp/Window.cpp
@@ -1,11 +1,87 @@
 #include "Window.h"
 
 Window::Window() : width_{800}, height_{600}
-{}
+{
+  initializeKeys_();
+}
 
 Window::Window(const GLint windowWidth, const GLint windowHeight) :
   width_{windowWidth}, height_{windowHeight}
-{}
+{
+  initializeKeys_();
+}
+
+void Window::initializeKeys_()
+{
+  for (auto &el : keys_)
+  {
+    el = false;
+  }
+}
+
+void Window::createCallbacks()
+{
+  glfwSetKeyCallback(pMainWindow_, handleKeys_);
+  glfwSetCursorPosCallback(pMainWindow_, handleMouse_);
+}
+
+GLfloat Window::getXChange()
+{
+  auto change = xChange_;
+  xChange_ = 0;
+  return change;
+}
+
+GLfloat Window::getYChange()
+{
+  auto change =yChange_;
+  yChange_ = 0;
+  return change;
+}
+
+void Window::handleKeys_(GLFWwindow *pWindow, int key, int code, int action, int mode)
+{
+  auto pTheWindow = static_cast<Window*>(glfwGetWindowUserPointer(pWindow));
+
+  if ((key == GLFW_KEY_ESCAPE) && (action == GLFW_PRESS))
+  {
+    glfwSetWindowShouldClose(pWindow, GL_TRUE);
+  }
+
+  if ((key >= 0) && (key < numberOfAsciiCharacters_))
+  {
+    if (action == GLFW_PRESS)
+    {
+      // Set the pressed key to true.
+      pTheWindow->keys_[key] = true;
+    }
+    else if (action == GLFW_RELEASE)
+    {
+      // Set the released key to false.
+      pTheWindow->keys_[key] = false;
+    }
+  }
+}
+
+void Window::handleMouse_(GLFWwindow *pWindow, double xPos, double yPos)
+{
+  auto pTheWindow = static_cast<Window*>(glfwGetWindowUserPointer(pWindow));
+
+  if (pTheWindow->isMouseMovedAtFirst_)
+  {
+    pTheWindow->lastX_ = (GLfloat)xPos;
+    pTheWindow->lastY_ = (GLfloat)yPos;
+    pTheWindow->isMouseMovedAtFirst_ = false;
+  }
+
+  pTheWindow->xChange_ = (GLfloat)xPos - pTheWindow->lastX_;
+
+  // Avoid inverted up and down.
+  pTheWindow->yChange_ = pTheWindow->lastY_ - (GLfloat)yPos;
+
+  pTheWindow->lastX_ = (GLfloat)xPos;
+  pTheWindow->lastY_ = (GLfloat)yPos;
+}
 
 int Window::initialize()
 {
@@ -41,6 +117,12 @@ int Window::initialize()
 
   // Set context for GLEW to use
   glfwMakeContextCurrent(pMainWindow_);
+  
+  // Handle key and mouse input.
+  createCallbacks();
+
+  // Lock the mouse cursor to the screen.
+  glfwSetInputMode(pMainWindow_, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
   // Allow modern extension features
   glewExperimental = GL_TRUE;
@@ -59,6 +141,9 @@ int Window::initialize()
 
   // Setup viewport size
   glViewport(0, 0, bufferWidth_, bufferHeight_);
+
+  // Used for call back to the handle key function. 
+  glfwSetWindowUserPointer(pMainWindow_, this);
 }
 
 Window::~Window()
--- OpenGL/Project/OpenGLCourseApp/OpenGLCourseApp/Window.h
+++ OpenGL/Project/OpenGLCourseApp/OpenGLCourseApp/Window.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include<array>
 #include <iostream>
 
 #include<GL\glew.h>
@@ -8,6 +9,9 @@
 
 class Window
 {
+private:
+  static const int numberOfAsciiCharacters_ = 1024;
+
 public:
   Window();
   Window(const GLint windowWidth, const GLint windowHeight);
@@ -16,6 +20,10 @@ public:
 
   GLfloat getBufferWidth() const { return (GLfloat)bufferWidth_; }
   GLfloat getBufferHeight() const { return (GLfloat)bufferHeight_; }
+  const std::array<bool, numberOfAsciiCharacters_> &getKeys() const { return keys_; }
+
+  GLfloat getXChange();
+  GLfloat getYChange();
 
   bool shouldWindowClose() const { return glfwWindowShouldClose(pMainWindow_); }
 
@@ -24,9 +32,25 @@ public:
   ~Window();
 
 private:
+// Private Methods
+  void initializeKeys_();
+  void createCallbacks();
+  static void handleKeys_(GLFWwindow *pWindow, int key, int code, int action, int mode);
+  static void handleMouse_(GLFWwindow *pWindow, double xPos, double yPos);
+
+// Private Attributes
   GLFWwindow *pMainWindow_;
+
   GLint width_;
   GLint height_;
   GLint bufferWidth_;
   GLint bufferHeight_;
+
+  GLfloat lastX_;
+  GLfloat lastY_;
+  GLfloat xChange_;
+  GLfloat yChange_;
+  bool isMouseMovedAtFirst_;
+
+  std::array<bool, numberOfAsciiCharacters_> keys_;
 };
