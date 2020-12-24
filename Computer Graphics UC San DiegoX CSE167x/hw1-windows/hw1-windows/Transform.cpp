1717a99cfe4933c170215a4ffb20163db80e2c7a
Arsha <arsha.shiri@gmail.com>
2020-12-24 18:57:58 +0100

    Added the solution for HW1

--- Computer Graphics UC San DiegoX CSE167x/hw1-windows/hw1-windows/Transform.cpp
+++ Computer Graphics UC San DiegoX CSE167x/hw1-windows/hw1-windows/Transform.cpp
@@ -1,33 +1,58 @@
 // Transform.cpp: implementation of the Transform class.
 
 #include "Transform.h"
+#include <cstdio>
 
 //Please implement the following functions:
 
 // Helper rotation function.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) {
-  // YOUR CODE FOR HW1 HERE
+  const auto rad = glm::radians(degrees);
+  const auto cosAngle = cos(rad);
+  const auto sinAngle = sin(rad);
 
-  // You will change this return call
-  return mat3();
+  const auto x = axis[0];
+  const auto y = axis[1];
+  const auto z = axis[2];
+
+  const auto identityMatrix = mat3(1.0);
+
+  const auto firstComponent = cosAngle * identityMatrix;
+  const auto secondComponent = (1 - cosAngle) * mat3(x * x, x * y, x * z,
+                                                     x * y, y * y, y * z,
+                                                     x * z, y * z, z * z);
+  const auto thirdComponent = sinAngle * mat3(0.f, -z, y,
+                                              z, 0.f, -x,
+                                              -y, x, 0.f);
+
+  return firstComponent + secondComponent + thirdComponent;
 }
 
 // Transforms the camera left around the "crystal ball" interface
 void Transform::left(float degrees, vec3& eye, vec3& up) {
-  // YOUR CODE FOR HW1 HERE
+  const auto rotationMatrix = rotate(degrees, -up);
+  eye = rotationMatrix * eye;
+  up = rotationMatrix * up;
 }
 
 // Transforms the camera up around the "crystal ball" interface
 void Transform::up(float degrees, vec3& eye, vec3& up) {
-  // YOUR CODE FOR HW1 HERE 
+  const auto rotationAxis = normalize(cross(up, eye));
+  const auto rotationMatrix = rotate(degrees, rotationAxis);
+  eye = rotationMatrix * eye;
+  up = rotationMatrix * up;
 }
 
 // Your implementation of the glm::lookAt matrix
 mat4 Transform::lookAt(vec3 eye, vec3 up) {
-  // YOUR CODE FOR HW1 HERE
+  const auto w = normalize(eye);
+  const auto u = normalize(cross(up, w));
+  const auto v = cross(w, u);
 
-  // You will change this return call
-  return mat4();
+  return mat4(u[0], v[0], w[0], 0.f,
+              u[1], v[1], w[1], 0.f,
+              u[2], v[2], w[2], 0.f,
+              -dot(u, eye), -dot(v, eye), -dot(w, eye), 1.f);
 }
 
 Transform::Transform()
