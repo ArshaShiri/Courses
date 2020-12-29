66a90c85d6c35ec102c6a4fd722c5a1fbdc824b3
Arsha <arsha.shiri@gmail.com>
2020-12-29 13:07:12 +0100

    Added the missing functionalities in transform.cpp Fixed some bugs from HW1

--- Computer Graphics UC San DiegoX CSE167x/hw2-windows/hw2-windows/Transform.cpp
+++ Computer Graphics UC San DiegoX CSE167x/hw2-windows/hw2-windows/Transform.cpp
@@ -9,54 +9,86 @@
 // Helper rotation function.  Please implement this.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) 
 {
-    mat3 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Please implement this.  Likely the same as in HW 1.  
-    return ret;
+  const auto rad = glm::radians(degrees);
+  const auto cosAngle = cos(rad);
+  const auto sinAngle = sin(rad);
+  const auto normalizedAxis = normalize(axis);
+
+  const auto x = normalizedAxis[0];
+  const auto y = normalizedAxis[1];
+  const auto z = normalizedAxis[2];
+
+  const auto identityMatrix = mat3(1.f);
+
+  const auto firstComponent = cosAngle * identityMatrix;
+  const auto secondComponent = (1 - cosAngle) * mat3(x * x, x * y, x * z,
+                                                     x * y, y * y, y * z,
+                                                     x * z, y * z, z * z);
+  const auto thirdComponent = sinAngle * mat3(0.f, z, -y,
+                                              -z, 0.f, x,
+                                              y, -x, 0.f);
+
+  return firstComponent + secondComponent + thirdComponent;
 }
 
 void Transform::left(float degrees, vec3& eye, vec3& up) 
 {
-    // YOUR CODE FOR HW2 HERE
-    // Likely the same as in HW 1.  
+  const auto rotationMatrix = rotate(degrees, up);
+  eye = rotationMatrix * eye;
+  up = normalize(rotationMatrix * up);
 }
 
 void Transform::up(float degrees, vec3& eye, vec3& up) 
 {
-    // YOUR CODE FOR HW2 HERE 
-    // Likely the same as in HW 1.  
+  const auto rotationAxis = normalize(cross(eye, up));
+  const auto rotationMatrix = rotate(degrees, rotationAxis);
+  eye = rotationMatrix * eye;
+  up = normalize(rotationMatrix * up);
 }
 
 mat4 Transform::lookAt(const vec3 &eye, const vec3 &center, const vec3 &up) 
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Likely the same as in HW 1.  
-    return ret;
+  const auto w = normalize(eye - center);
+  const auto u = normalize(cross(up, w));
+  const auto v = cross(w, u);
+
+  return mat4(u[0], v[0], w[0], 0.f,
+              u[1], v[1], w[1], 0.f,
+              u[2], v[2], w[2], 0.f,
+              -dot(u, eye), -dot(v, eye), -dot(w, eye), 1.f);
 }
 
 mat4 Transform::perspective(float fovy, float aspect, float zNear, float zFar)
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // New, to implement the perspective transform as well.  
-    return ret;
+  const auto theta = fovy / 2.f;
+  const auto d = 1.f / tan(glm::radians(theta));
+  const auto A = -(zFar + zNear) / (zFar - zNear);
+  const auto B = (-2.f * zFar * zNear) / (zFar - zNear);
+
+  return mat4(d / aspect, 0.f, 0.f, 0.f,
+              0.f, d, 0.f, 0.f,
+              0.f, 0.f, A, -1.f,
+              0.f, 0.f, B, 0.f);
 }
 
 mat4 Transform::scale(const float &sx, const float &sy, const float &sz) 
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Implement scaling 
-    return ret;
+  auto ret = mat4(1.0);
+  ret[0][0] = sx;
+  ret[1][1] = sy;
+  ret[2][2] = sz;
+
+  return ret;
 }
 
 mat4 Transform::translate(const float &tx, const float &ty, const float &tz) 
 {
-    mat4 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Implement translation 
-    return ret;
+  auto ret = mat4(1.0);
+  ret[3][0] = tx;
+  ret[3][1] = ty;
+  ret[3][2] = tz;
+
+  return ret;
 }
 
 // To normalize the up direction and construct a coordinate frame.  
