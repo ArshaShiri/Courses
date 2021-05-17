d7b09397e5252fed9910f10be8eaa2db0fbe8c14
Arsha <arsha.shiri@gmail.com>
2021-05-17 20:50:56 +0200

    Initial commit for Embedded C course, some initial projects are generated. The rest are the downloaded

---
+++ Embedded/Embedded-C/My_workspace/host/TestProject/main.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: May 10, 2021
+ *      Author: arsha
+ *
+ *
+ */
+
+#include<stdio.h>
+
+int main(void)
+{
+  printf("Hello world\n");
+
+  printf("Press 'Enter' to exit this application\n");
+
+  getchar();
+
+  return 0;
+}
+
+
