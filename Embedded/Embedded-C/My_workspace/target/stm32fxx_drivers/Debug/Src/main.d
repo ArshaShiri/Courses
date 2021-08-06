cdb82684aa4159685e50e856519527628197fe11
Arsha <arsha.shiri@gmail.com>
2021-08-06 20:39:46 +0200

    Added more driver stuff

--- Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/Src/main.d
+++ Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/Src/main.d
@@ -1,1 +1,4 @@
-Src/main.o: ../Src/main.c
+Src/main.o: ../Src/main.c \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h:
---
+++ Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/drivers/Src/stm32f407xx_gpio_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f407xx_gpio_driver.o: \
+ ../drivers/Src/stm32f407xx_gpio_driver.c \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h:
