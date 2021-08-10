a68297e04ee47475db533b6c6f3f93f9257515fa
Arsha <arsha.shiri@gmail.com>
2021-08-10 08:33:54 +0200

    Added a layer for population and layer control

--- Python/Web_Map/map.py
+++ Python/Web_Map/map.py
@@ -16,15 +16,25 @@ longitude = list(data["LON"])
 elevations = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-featureGroup = folium.FeatureGroup(name="My Map")
+volcanoesFeatureGroup = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(latitude, longitude, elevations):
-    featureGroup.add_child(folium.CircleMarker(location=[lt, ln],
+    volcanoesFeatureGroup.add_child(folium.CircleMarker(location=[lt, ln],
                                                radius=6,
                                                popup="Elevation: " + str(el) + "m",
                                                fill_color=getColorBasedOnElevation(el),
                                                color="grey",
                                                fill_opacity=0.7))
+
+populationFeatureGroup = folium.FeatureGroup(name="Population")                                               
+
+populationFeatureGroup.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(), 
+                                      style_function=lambda x: {"fillColor":"yellow" if x["properties"]["POP2005"] < 10000000 
+                                                                                     else "orange" if 10000000 <= x["properties"]["POP2005"] < 20000000 
+                                                                                     else "red"}))                                                                                     
     
-map.add_child(featureGroup)
+map.add_child(volcanoesFeatureGroup)
+map.add_child(populationFeatureGroup)
+map.add_child(folium.LayerControl())
+
 map.save("map.html")
