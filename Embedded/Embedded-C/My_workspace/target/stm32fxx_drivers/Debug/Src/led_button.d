12f33cefb9f53ccd2f28cb81c956f788d140e337
Arsha <arsha.shiri@gmail.com>
2021-08-10 19:46:25 +0200

    Added some LED examples using the drivers

---
+++ Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/Src/led_button.d
@@ -0,0 +1,10 @@
+Src/led_button.o: ../Src/led_button.c \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h:
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx_gpio_driver.h:
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h:
--- Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/Src/subdir.mk
+++ Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/Src/subdir.mk
@@ -5,22 +5,32 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../Src/led_button.c \
+../Src/led_toggle.c \
 ../Src/main.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
+./Src/led_button.o \
+./Src/led_toggle.o \
 ./Src/main.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
+./Src/led_button.d \
+./Src/led_toggle.d \
 ./Src/main.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
+Src/led_button.o: ../Src/led_button.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/led_button.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Src/led_toggle.o: ../Src/led_toggle.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/led_toggle.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/main.o: ../Src/main.c Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Src/main.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 Src/syscalls.o: ../Src/syscalls.c Src/subdir.mk
