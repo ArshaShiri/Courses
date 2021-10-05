945033a0c9464ee3828ba590e7ee388753c1312c
Arsha <arsha.shiri@gmail.com>
2021-10-05 19:58:39 +0200

    Added I2C drivers

---
+++ Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/drivers/Src/stm32f407xx_i2c_driver.d
@@ -0,0 +1,8 @@
+drivers/Src/stm32f407xx_i2c_driver.o: \
+ ../drivers/Src/stm32f407xx_i2c_driver.c \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h \
+ /home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx_i2c_driver.h:
+
+/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc/stm32f407xx.h:
--- Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/drivers/Src/subdir.mk
+++ Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/Debug/drivers/Src/subdir.mk
@@ -6,20 +6,25 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../drivers/Src/stm32f407xx_gpio_driver.c \
+../drivers/Src/stm32f407xx_i2c_driver.c \
 ../drivers/Src/stm32f407xx_spi_driver.c 
 
 OBJS += \
 ./drivers/Src/stm32f407xx_gpio_driver.o \
+./drivers/Src/stm32f407xx_i2c_driver.o \
 ./drivers/Src/stm32f407xx_spi_driver.o 
 
 C_DEPS += \
 ./drivers/Src/stm32f407xx_gpio_driver.d \
+./drivers/Src/stm32f407xx_i2c_driver.d \
 ./drivers/Src/stm32f407xx_spi_driver.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 drivers/Src/stm32f407xx_gpio_driver.o: ../drivers/Src/stm32f407xx_gpio_driver.c drivers/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/stm32f407xx_gpio_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+drivers/Src/stm32f407xx_i2c_driver.o: ../drivers/Src/stm32f407xx_i2c_driver.c drivers/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/stm32f407xx_i2c_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 drivers/Src/stm32f407xx_spi_driver.o: ../drivers/Src/stm32f407xx_spi_driver.c drivers/Src/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32 -DSTM32F407G_DISC1 -DSTM32F4 -DSTM32F407VGTx -c -I../Inc -I"/home/arsha/Repos/Courses/Embedded/Embedded-C/My_workspace/target/stm32fxx_drivers/drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/stm32f407xx_spi_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
